17b4ec22dafeaa95173c1a05503626c1cfd678f8
HWS <hwas2620@gmail.com>
2025-12-23 17:29:42 +0900

    fix: raw_points의 시간 계산 로직 개선

--- src/mappers/raw-points.mapper.ts
+++ src/mappers/raw-points.mapper.ts
@@ -5,10 +5,13 @@ export function toViewportPointerEvents(
   raw_points: RawPointerEvent[],
 ): PointerEvent[] {
   console.log("raw_points", raw_points);
+
+  const start_time = raw_points[0].t;
+
   return raw_points.map((p) => [
     p.viewport_p.x,
     p.viewport_p.y,
-    p.t,
+    p.t - start_time,
     p.event_type,
   ]);
 }
