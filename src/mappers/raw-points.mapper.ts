b47c73cd63d0b0d885c0baa0575459a98a4aa450
HWS <hwas2620@gmail.com>
2025-12-23 14:42:51 +0900

    fix: RawPointerEventModel 매핑 로직 개선

--- src/mappers/raw-points.mapper.ts
+++ src/mappers/raw-points.mapper.ts
@@ -1,9 +1,14 @@
 import type { PointerEvent } from "@/types/contracts/behavior";
-import { RawPointerEvent } from "@/types/input-event/RawPointerEventModel";
+import { RawPointerEvent } from "@/ui/types/RawPointerEventModel";
 
 export function toViewportPointerEvents(
   raw_points: RawPointerEvent[],
 ): PointerEvent[] {
   console.log("raw_points", raw_points);
-  return raw_points.map((p) => [p.viewport_x, p.viewport_y, p.t, p.event_type]);
+  return raw_points.map((p) => [
+    p.viewport_p.x,
+    p.viewport_p.y,
+    p.t,
+    p.event_type,
+  ]);
 }
