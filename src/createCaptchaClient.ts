9db5a994f0db34c49c950789e70a75b03b7c5553
HWS <hwas2620@gmail.com>
2025-12-25 15:25:21 +0900

    feat: 빌드 경량화

--- src/createCaptchaClient.ts
+++ src/createCaptchaClient.ts
@@ -1,5 +1,5 @@
 import { CaptchaClient } from "./api/CaptchaClient";
-import { MockCaptchaClient } from "./api/MockCaptchaClient";
+//import { MockCaptchaClient } from "./api/MockCaptchaClient";
 import { HttpCaptchaClient } from "./api/HttpCaptchaClient";
 import { getEnv } from "./config/captcha.env";
 
@@ -8,7 +8,7 @@ export function createCaptchaClient(): CaptchaClient {
   console.log("env", env);
   switch (env.mode) {
     case "dev":
-      return new MockCaptchaClient();
+    //return new MockCaptchaClient();
     case "local":
     case "prod":
       return new HttpCaptchaClient(env);
